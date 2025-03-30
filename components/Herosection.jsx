aec134c52c728c5b03e81a0f276960c1024e0ded
Rohit Patil <rohitrajupatil12@gmail.com>
2025-03-30 12:03:35 +0530

    recreate the herosection

--- components/Herosection.jsx
+++ components/Herosection.jsx
@@ -5,27 +5,11 @@ import "./hero.css";
 function Herosection() {
   return (
     <>
-      <main>
-        <div className="container-box">
-          <div className="main-container">
-            {data.map((item) => {
-              console.log(item); // Move console.log outside JSX
+    <main>
+      <div></div>
+    </main>
 
-              return (
-                <div className="main-box-contianer">
-                  <div className="country-box" key={item.id}>
-                    <img src={item.flags.svg} alt={item.name} />{" "}
-                    {/* ✅ Fixed img src */}
-                    <h2>{item.name}</h2>
-                    <p>Capital: {item.capital}</p>
-                    {/* ✅ Fixed the label for clarity */}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </main>
+
     </>
   );
 }
