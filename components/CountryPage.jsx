1e1c7381b9a810ff65c4991bdd9b670fdc23db79
Rohit Patil <rohitrajupatil12@gmail.com>
2025-03-31 09:32:19 +0530

    css add and hard coding is done and reposive css is remaining

--- components/CountryPage.jsx
+++ components/CountryPage.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./countrypage.css";
 
 function CountryPage() {
   return (
     <>
-    <div className='main-country-page-container'>
-        <div className='page-image-container'>
-            <img src="https://flagcdn.com/af.svg" alt="" />
+      <div className="main-country-page-container">
+        <div className="page-image-container">
+          <img src="https://flagcdn.com/af.svg" alt="" />
         </div>
-        <div className='country-details-page'>
-            <div className='detail-right'>
-                <h2>name</h2>
-                <h2>population</h2>
-                <h2>region</h2>
-                <h2>capital</h2>
-            </div>
-            <div className='detail-left'>
-                <h2>Domain</h2>
-                <h2>sub region</h2>
-                <h2>area</h2>
-                <h2>bordering country</h2>
-            </div>
+        <div className="country-details-page">
+          <div className="detail-right">
+            <h2>name</h2>
+            <h2>population</h2>
+            <h2>region</h2>
+            <h2>capital</h2>
+          </div>
+          <div className="detail-left">
+            <h2>Domain</h2>
+            <h2>sub region</h2>
+            <h2>area</h2>
+            <h2>bordering country</h2>
+          </div>
         </div>
-    </div>
-    
+      </div>
     </>
-  )
+  );
 }
 
-export default CountryPage
+export default CountryPage;
