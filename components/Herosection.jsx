f7e88d74ddd9442f2615f85cb2339ecb37255cb3
Rohit Patil <rohitrajupatil12@gmail.com>
2025-03-31 08:30:51 +0530

    add the herosection adn it half css

--- components/Herosection.jsx
+++ components/Herosection.jsx
@@ -6,7 +6,20 @@ function Herosection() {
   return (
     <>
     <main>
-      <div></div>
+      <div className="container-box">
+        {
+          data.map((list ,index) => {
+            return(
+              <div className="country-box" key={index}>
+                <img src={list.flags.svg} alt="list.name" />
+                <h2> Name :{list.name}</h2>
+                <h2> Population:{list.population}</h2>
+                <h2>Region:{list.region}</h2>
+              </div>
+            )
+          })
+        }
+      </div>
     </main>
 
 
