3c618182a096547189d437dee05763ab1e77904e
Rohit Patil <rohitrajupatil12@gmail.com>
2025-03-31 22:49:51 +0530

    add the filter option and css of it , but completed the css of filer component

--- components/FilterElement.jsx
+++ components/FilterElement.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FilerElement.css";
 
 function FilterElement() {
+  const [menu, setmenu] = useState(false);
+
+  const filerONOff = () => {
+    setmenu(!menu);
+  };
+
   return (
     <>
       <div className="filter-element">
-        <input type="text" className="input-tag" placeholder="Enter the Country Name" />
+        <input
+          type="text"
+          className="input-tag"
+          placeholder="Enter the Country Name"
+        />
 
         <div className="filer-box">
-          <button className="filter-btn">Filter</button>
+          <button onClick={filerONOff} className="filter-btn">Filter</button>
+          {
+            menu && (
+              <div className="filerOption">
+                <ul>
+                  {
+                    ["Asia", "Africa", "Americas", "Europe"].map((regionName) => {
+                      return(
+                        <li key={regionName} onClick={() => setmenu(regionName)}>{regionName}</li>
+                      )
+                    })
+                  }
+                </ul>
+              </div>
+            )
+          }
         </div>
       </div>
     </>
